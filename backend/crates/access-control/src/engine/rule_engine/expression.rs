8aa43cde50cc81fc7258c66a73a082e98a2e1cc3
RP <93403502+confused-moniker@users.noreply.github.com>
2025-12-23 21:03:50 -0600

    Plug Pip (#356)
    * mod
    
    * impl for pip plug
    
    * mod
    
    * od
    
    * mod

--- backend/crates/access-control/src/engine/rule_engine/expression.rs
+++ backend/crates/access-control/src/engine/rule_engine/expression.rs
@@ -1,32 +1,12 @@
-use crate::context::PolicyContext;
+use crate::{context::PolicyContext, engine::rule_engine::pip::pip};
 use haste_fhir_client::FHIRClient;
 use haste_fhir_model::r4::generated::{
-    resources::{AccessPolicyV2, Resource},
-    terminology::IssueType,
-    types::Expression,
+    resources::AccessPolicyV2, terminology::IssueType, types::Expression,
 };
 use haste_fhir_operation_error::OperationOutcomeError;
 use haste_fhirpath::{Context, ExternalConstantResolver, FHIRPathError};
 use haste_pointer::Pointer;
 use std::sync::Arc;
-
-async fn resolve_variable<'a, CTX, Client: FHIRClient<CTX, OperationOutcomeError>>(
-    _context: Arc<PolicyContext<CTX, Client>>,
-    _pointer: Pointer<AccessPolicyV2, AccessPolicyV2>,
-) -> Result<Option<Resource>, OperationOutcomeError> {
-    // let patient = context
-    //     .client
-    //     .read(
-    //         context.client_context,
-    //         ResourceType::Patient,
-    //         "asdf".to_string(),
-    //     )
-    //     .await?;
-
-    // Ok(patient)
-
-    Ok(None)
-}
 
 pub fn create_config<
     'a,
@@ -38,11 +18,23 @@ pub fn create_config<
 ) -> haste_fhirpath::Config<'a> {
     haste_fhirpath::Config {
         variable_resolver: Some(ExternalConstantResolver::Function(Box::new(
-            move |_variable_id: String| {
+            move |variable_id: String| {
                 let pointer = pointer.clone();
                 let context = context.clone();
                 Box::pin(async move {
-                    let _p = resolve_variable(context, pointer).await;
+                    let Ok(access_policy_v2) = pointer.value().ok_or_else(|| {
+                        OperationOutcomeError::fatal(
+                            IssueType::Invalid(None),
+                            "Pointer root does not contain an AccessPolicyV2 resource.".to_string(),
+                        )
+                    }) else {
+                        return None;
+                    };
+
+                    let Some(_result) = pip(context, access_policy_v2, &variable_id).await.ok()
+                    else {
+                        return None;
+                    };
 
                     None
                 })
--- backend/crates/access-control/src/engine/rule_engine/pip.rs
+++ backend/crates/access-control/src/engine/rule_engine/pip.rs
@@ -1,25 +1,65 @@
 //! Policy Information Point (PIP) module for the access control engine.
 //! This module is responsible for retrieving contextual information that can be used during policy evaluation.
 use haste_fhir_client::FHIRClient;
-use haste_fhir_model::r4::generated::resources::AccessPolicyV2;
+use haste_fhir_model::r4::generated::{
+    resources::{AccessPolicyV2, AccessPolicyV2Attribute},
+    terminology::AccessPolicyAttributeOperationTypes,
+};
 use haste_fhir_operation_error::OperationOutcomeError;
+use haste_reflect::MetaValue;
 use std::sync::Arc;
 
 use crate::context::PolicyContext;
 
-#[allow(unused)]
-pub struct PIPResult {}
+fn find_attribute<'a>(
+    access_policy: &'a AccessPolicyV2,
+    variable_id: &str,
+) -> Option<&'a AccessPolicyV2Attribute> {
+    access_policy.attribute.as_ref().and_then(|attributes| {
+        attributes
+            .iter()
+            .find(|a| a.attributeId.value.as_ref().map(|s| s.as_str()) == Some(variable_id))
+    })
+}
 
-#[allow(unused)]
 pub async fn pip<'a, CTX, Client: FHIRClient<CTX, OperationOutcomeError>>(
     _policy_context: Arc<PolicyContext<CTX, Client>>,
-    _policy: &AccessPolicyV2,
+    access_policy: &AccessPolicyV2,
     variable_id: &str,
-) -> Result<PIPResult, OperationOutcomeError> {
-    match variable_id {
-        "user" => None,
-        _ => Some("test".to_string()),
+) -> Result<Option<Box<dyn MetaValue>>, OperationOutcomeError> {
+    let Some(attribute) = find_attribute(access_policy, variable_id) else {
+        return Ok(None);
     };
 
-    Ok(PIPResult {})
+    let Some(attribute_operation) = &attribute.operation else {
+        return Err(OperationOutcomeError::fatal(
+            haste_fhir_model::r4::generated::terminology::IssueType::Invalid(None),
+            format!(
+                "Attribute operation is not specified for attribute '{}'.",
+                variable_id
+            ),
+        ));
+    };
+
+    match attribute_operation.type_.as_ref() {
+        AccessPolicyAttributeOperationTypes::Read(_) => {
+            println!("asdf");
+            Ok(None)
+        }
+        AccessPolicyAttributeOperationTypes::SearchSystem(_) => {
+            println!("custom operation");
+            Ok(None)
+        }
+        AccessPolicyAttributeOperationTypes::SearchType(_) => {
+            println!("custom operation");
+            Ok(None)
+        }
+        AccessPolicyAttributeOperationTypes::Null(_) => Err(OperationOutcomeError::fatal(
+            haste_fhir_model::r4::generated::terminology::IssueType::Invalid(None),
+            format!(
+                "Attribute operation type is not specified for attribute '{}'.",
+                variable_id
+            ),
+        )),
+    }
 }
