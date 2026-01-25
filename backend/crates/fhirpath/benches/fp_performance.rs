a5589bed7bad3ceb63f76981437b63026debe066
confused-moniker <ralph@iguhealth.app>
2026-01-24 22:41:18 -0600

    FP Benchmark + Blog Post (#431)

--- backend/crates/fhirpath/benches/fp_performance.rs
+++ backend/crates/fhirpath/benches/fp_performance.rs
@@ -46,11 +46,67 @@ fn parser_test_simple(c: &mut Criterion) {
     });
 }
 
+static SYNTHEA_PATIENT: &str = include_str!("../data/synthea_patient.json");
+
+static PATIENT_PARAMETER_EXPRESSIONS: &[&str] = &[
+    "Patient.active",
+    "Patient.address",
+    "Patient.address.city",
+    "Patient.address.country",
+    "Patient.address.postalCode",
+    "Patient.address.state",
+    "Patient.address.use",
+    "Patient.birthDate",
+    "(Patient.deceased as dateTime)",
+    "Patient.deceased.exists() and Patient.deceased != false",
+    "Patient.telecom.where(system='email')",
+    "Patient.name.family",
+    "Patient.gender",
+    "Patient.generalPractitioner",
+    "Patient.name.given",
+    "Patient.identifier",
+    "Patient.communication.language",
+    "Patient.link.other",
+    "Patient.name",
+    "Patient.managingOrganization",
+    "Patient.telecom.where(system='phone')",
+    "Patient.name",
+    "Patient.telecom",
+];
+
+async fn index_patient(
+    engine: &FPEngine,
+    p: &Patient,
+) -> Result<(), haste_fhirpath::FHIRPathError> {
+    for expr in PATIENT_PARAMETER_EXPRESSIONS {
+        let _result = engine.evaluate(expr, vec![p]).await?;
+    }
+
+    Ok(())
+}
+
+fn index_synthea_patient(c: &mut Criterion) {
+    let patient = haste_fhir_serialization_json::from_str::<
+        haste_fhir_model::r4::generated::resources::Patient,
+    >(SYNTHEA_PATIENT)
+    .unwrap();
+
+    let engine = FPEngine::new();
+
+    c.bench_function("index_synthea_patient", |b| {
+        b.to_async(Runtime::new().unwrap())
+            .iter(|| index_patient(&engine, &patient))
+    });
+}
+
+criterion_group!(evaluation_tests, index_synthea_patient);
+
 criterion_group!(
-    benches,
+    parser_tests,
     fp_performance_simple,
     parser_test_performance,
     parser_test_complex,
     parser_test_simple
 );
-criterion_main!(benches);
+
+criterion_main!(evaluation_tests, parser_tests);
